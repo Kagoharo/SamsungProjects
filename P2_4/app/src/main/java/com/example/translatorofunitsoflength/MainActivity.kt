72532ca95843b2efcb16aa19ed622a081d729a29
Kagoharo <40521742+Kagoharo@users.noreply.github.com>
2022-01-12 12:42:27 +0300

    Update MainActivity.kt

--- P2_4/app/src/main/java/com/example/translatorofunitsoflength/MainActivity.kt
+++ P2_4/app/src/main/java/com/example/translatorofunitsoflength/MainActivity.kt
@@ -14,16 +14,11 @@ class MainActivity : AppCompatActivity() {
 
     var text: TextView? = null
     var image: ImageView? = null
-    var kmText: TextView? = null
-    var kmDouble: Double? = null
-    var mText: TextView? = null
-    var mDouble: Double? = null
-    var dmText: TextView? = null
-    var dmDouble: Double? = null
-    var cmText: TextView? = null
-    var cmDouble: Double? = null
-    var mmText: TextView? = null
-    var mmDouble: Double? = null
+    var kmT: TextView? = null
+    var mT: TextView? = null
+    var dmT: TextView? = null
+    var cmT: TextView? = null
+    var mmT: TextView? = null
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +26,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         text = findViewById(R.id.rezult)
         image = findViewById(R.id.answer)
-        kmText = findViewById(R.id.km)
-        mText = findViewById(R.id.m)
-        dmText = findViewById(R.id.dm)
-        cmText = findViewById(R.id.cm)
-        mmText = findViewById(R.id.mm)
+        kmT = findViewById(R.id.km)
+        mT = findViewById(R.id.m)
+        dmT = findViewById(R.id.dm)
+        cmT = findViewById(R.id.cm)
+        mmT = findViewById(R.id.mm)
 
         image?.setOnTouchListener { view, motionEvent ->
             if(motionEvent.getAction()==MotionEvent.ACTION_DOWN)
@@ -49,23 +44,21 @@ class MainActivity : AppCompatActivity() {
 
     fun buttonClick(v: View) {
 
-        val b: Button = findViewById(R.id.inspect)
+        val b: Button = findViewById(R.id.calculate)
 
         b.setOnClickListener {
-            kmDouble = kmText?.text.toString().toDouble()
-            mDouble = mText?.text.toString().toDouble()
-            dmDouble = dmText?.text.toString().toDouble()
-            cmDouble = cmText?.text.toString().toDouble()
-            mmDouble = mmText?.text.toString().toDouble()
+            val km = kmT?.text.toString().toDouble()
+            val m = mT?.text.toString().toDouble()
+            val dm = dmT?.text.toString().toDouble()
+            val cm = cmT?.text.toString().toDouble()
+            val mm = mmT?.text.toString().toDouble()
 
-            if ((kmDouble!! * 1000 == mDouble) and (dmDouble!! / 10 == mDouble) and (cmDouble!! / 100 == mDouble) and (mmDouble!! / 1000 == mDouble)) {
+            if ((km * 1000 == m) && (dm / 10 == m) && (cm / 100 == m) && (mm / 1000 == m)) {
                 text?.text = "Отлично!"
                 image?.setImageResource(R.drawable.good)
-                text?.setTextColor(Color.parseColor("#0000FF"));
             } else {
                 text?.text = "Повторите материал, ответ неверный!"
                 image?.setImageResource(R.drawable.bad)
-                text?.setTextColor(Color.parseColor("#FF0000"));
             }
         }
     }
